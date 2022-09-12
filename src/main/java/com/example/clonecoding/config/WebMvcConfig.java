8005749b5ca4a3064e80ba80ebddedb7fabc152b
sohyuneeee <sohyuney@naver.com>
2022-09-12 13:48:00 +0900

    cors 설정

--- src/main/java/com/example/clonecoding/config/WebMvcConfig.java
+++ src/main/java/com/example/clonecoding/config/WebMvcConfig.java
@@ -1,17 +1,12 @@
 package com.example.clonecoding.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("POST", "GET", "PUT", "DELETE");
-    }
-}
+//@Configuration
+//@EnableWebMvc
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedMethods("POST", "GET", "PUT", "DELETE");
+//    }
+//}
