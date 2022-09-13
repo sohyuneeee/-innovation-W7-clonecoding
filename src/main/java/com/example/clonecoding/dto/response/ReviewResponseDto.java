2b7834f283efef4b3d3d24f3ce41e8dba9d892da
sohyuneeee <sohyuney@naver.com>
2022-09-13 18:05:50 +0900

    수강평 reponse에 lectureId 추가

--- src/main/java/com/example/clonecoding/dto/response/ReviewResponseDto.java
+++ src/main/java/com/example/clonecoding/dto/response/ReviewResponseDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ReviewResponseDto {
 
+    private Long lectureId;
     private Long id;
     private String content;
     private String nickname;
@@ -21,6 +22,7 @@ public class ReviewResponseDto {
     private LocalDateTime createdAt;
 
     public ReviewResponseDto(Review review) {
+        this.lectureId = review.getLecture().getId();
         this.id = review.getId();
         this.nickname = review.getNickname();
         this.content= review.getContent();
